ad49c78c4dc82c894239c55a23c7ce11e5ba13e8
Tl <ganeshghodke7050@gmail.com>
2022-07-06 16:00:52 +0530

    Project initial commit

---
+++ src/main/java/com/ganesh/GitPraciceProjectApplication.java
@@ -0,0 +1,13 @@
+package com.ganesh;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitPraciceProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GitPraciceProjectApplication.class, args);  
+	}
+
+}
