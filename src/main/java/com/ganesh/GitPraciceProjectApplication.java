0e4ef84b80b500b69e6107211b8cf43ef7a0d8c3
Tl <ganeshghodke7050@gmail.com>
2022-07-06 16:59:57 +0530

    Displayed message

--- src/main/java/com/ganesh/GitPraciceProjectApplication.java
+++ src/main/java/com/ganesh/GitPraciceProjectApplication.java
@@ -9,6 +9,7 @@ public class GitPraciceProjectApplication {
 
 	public static void main(String[] args) {
 		ConfigurableApplicationContext context = SpringApplication.run(GitPraciceProjectApplication.class, args);
+		System.out.println("Welcome to the git project");
 		context.close();
 	}
 
