0952ce7546f31699f75c863c8f2bf165b5d96aa2
Tl <ganeshghodke7050@gmail.com>
2022-07-07 23:56:40 +0530

    HIS-301:Related changes

--- src/main/java/com/ganesh/GitPraciceProjectApplication.java
+++ src/main/java/com/ganesh/GitPraciceProjectApplication.java
@@ -6,13 +6,18 @@ import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
 public class GitPraciceProjectApplication {
-
+	private Map<String, Object> cache = new HashMap<>();
 	public static void main(String[] args) {
 		ConfigurableApplicationContext context = SpringApplication.run(GitPraciceProjectApplication.class, args);
 		System.out.println("Welcome to the git project");
 		// Working on HIS-122
 		int a=10;
 		context.close();
+	}
+	
+	//HIS-301 changes
+	public void loadDataToCache(){
+		//logic
 	}
 
 }
